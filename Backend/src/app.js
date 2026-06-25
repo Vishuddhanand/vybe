9cd750377687aab181952890e6c8d05f9fd70c5b
Vishuddhanand <vishupillewan2006@gmail.com>
2026-06-25 23:29:30 +0530

    Refactor app.js to improve code organization and readability

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
-
+const path = require("path")
 
 const app = express()
+
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
